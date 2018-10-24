f6d4dc6f8cccceca0fd07119ff26277da521a00d
Roger-ELIAS <roger.elias@etu.univ-amu.fr>
2018-10-24 09:59:37 +0200

    Distance Levenshtein et trigramme

--- src/DistanceLevenshtein.java
+++ src/DistanceLevenshtein.java
@@ -1,21 +1,36 @@
 public class DistanceLevenshtein {
 
     public static int distance(String mot, String mot2 ) {
-        int [] opt = new int[mot.length()];
+        int [][] opt = new int[mot.length()+1][mot2.length()+1];
 
         for(int j = 0; j<opt.length;j++)
-            opt[j]=j;
+            opt[j][0]=j;
+        for(int j = 0; j<opt[0].length;j++)
+            opt[0][j]=j;
 
-        for(int i =1; i<mot.length();i++){
-            opt[0]=1;
-            for(int k=i; k< mot2.length();k++){
-                int resultat;
+        for(int i =0; i<mot.length();i++){
+            for(int k=0; k< mot2.length();k++){
+                int cpt = 1;
+                if (mot.substring(i,i+1).equals(mot2.substring(k,k+1))){
+                    cpt = 0;
+                }
+                //System.out.println((mot.substring(i,i+1) +"=="+ mot2.substring(k,k+1)));
+                //System.out.println("cpt"+cpt);
+                //System.out.println(Math.min(Math.min(opt[i][k],opt[i][k+1]),opt[i+1][k]));
+                opt[i+1][k+1]=Math.min(Math.min(opt[i][k],opt[i][k+1]),opt[i+1][k])+cpt;
+
+
             }
 
         }
-
-
-        return 0;
+        /*
+        for(int j = 0; j<mot.length()+1;j++){
+            for(int i = 0; i<mot2.length()+1;i++)
+                System.out.print("|"+opt[j][i]);
+            System.out.println("|");
+        }
+        */
+        return opt[mot.length()][mot2.length()];
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,53 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Main {
+
+    public static List<String> trigrames(String mot){
+        List<String> list = new ArrayList<String>();
+
+        String motchevrons = '<'+mot+'>';
+        for (int i=1;i<motchevrons.length()-1;++i){
+            list.add(motchevrons.substring(i-1,i+2));
+        }
+        //System.out.println(list);
+        return list;
+    }
 
     public static void main(String[] args) {
         String [] deuxmots = {"chien", "schium"};
+        System.out.println(DistanceLevenshtein.distance("honda","hyundai"));
+        trigrames("coucou");
+
+        ArrayList<String> dico = new ArrayList<String>();
+        HashSet<String> trigs = new HashSet<String>();
+        HashMap<String,List<String>> trigcomm = new HashMap<String,List<String>>();
+
+        Scanner sc = null;
+        try {
+            sc = new Scanner(new File("dico.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        while (sc.hasNext()) {
+            String str = sc.next();
+            dico.add(str);
+            trigs.addAll(trigrames(str));
+        }
+
+        for(String trig: trigs){
+            List<String> motconttrig = new ArrayList<String>();
+            for(String mot: dico){
+                if(trigrames(mot).contains(trig))
+                    motconttrig.add(mot);
+            }
+            trigcomm.put(trig,motconttrig);
+        }
+
+
+        //dico.put(trigrames("coucou"),"coucou");
+        System.out.println(dico);
+        System.out.println(trigcomm);
     }
 }
